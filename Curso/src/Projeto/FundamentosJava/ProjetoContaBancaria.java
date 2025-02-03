2431d818778a1d3b6bb2247e0b530736ff75e9f8
Luiz Mendes <luizmendes0897@gmail.com>
2025-02-03 09:10:37 -0300

    CursoDioJava

---
+++ Curso/src/Projeto/FundamentosJava/ProjetoContaBancaria.java
@@ -0,0 +1,15 @@
+package Projeto.FundamentosJava;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProjetoContaBancaria {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("Digite seu nome:");
+		String nome = scanner.next();
+		
+		
+		
+	}
+}
